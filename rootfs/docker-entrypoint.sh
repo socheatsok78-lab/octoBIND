6dbaabc9ee5cddcc33bf344c32d4fca7d5e8986a
Socheat Sok <socheatsok78@gmail.com>
2023-01-31 13:11:20 +0700

    Revert "Add allow-transfer and allow-update to secondary zone"
    This reverts commit c52b2f426e231c0c71381a4d958ff7abdcf35f31.

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -182,12 +182,6 @@ zone "${zone}." {
   type ${NS_ROLE};
   file "${ZONE_DATABASE}";
   primaries { ${NS_1_SERVER}; };
-  allow-transfer {
-    key "${OCTODNS_KEY_NAME}"; # AXFR
-  };
-  allow-update {
-    key "${OCTODNS_KEY_NAME}"; # RFC 2136
-  };
 };
 
 EOF
