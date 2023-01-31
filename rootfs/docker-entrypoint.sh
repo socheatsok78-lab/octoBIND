e32dff2e189772792bf14e41123dfd0b9803417b
Socheat Sok <socheatsok78@gmail.com>
2023-01-31 12:15:39 +0700

    Update primary and secondary server transfer methods

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 # Bind ENV
+BIND_USER="bind"
 NAMED_CONF_FILE="${NAMED_CONF_FILE:-/etc/bind/named.conf.local}"
 NAMED_CONF_BACKUP_FILE="${NAMED_CONF_FILE}.origin"
 
@@ -28,17 +29,18 @@ fi
 # Add OCTODNS_KEY to NAMED_CONF_FILE
 echo "// OctoDNS Key" >> "${NAMED_CONF_FILE}"
 cat "${OCTODNS_KEY_FILE}" >> "${NAMED_CONF_FILE}"
+chown "${BIND_USER}:${BIND_USER}" "${NAMED_CONF_FILE}"
 
 # ---
 
 NS_DOMAIN="${NS_DOMAIN:-nameserver.local}"
 NS_ROLE=${NS_ROLE:-primary} # role: primary or secondary
 NS_DATABASE="/var/lib/bind/db.${NS_DOMAIN}"
-NS_ADDR_COUNT=${NS_ADDR_COUNT:-1}
 
-# NS1_ADDR=192.168.0.151
-# NS2_ADDR=192.168.0.152
-# NS3_ADDR=192.168.0.153
+NS_SERVER_COUNT=${NS_SERVER_COUNT:-1}
+# c=192.168.0.151
+# c=192.168.0.152
+# c=192.168.0.153
 
 # Generate NS_DOMAIN zone
 cat <<EOF > "${NS_DATABASE}"
@@ -47,23 +49,37 @@ cat <<EOF > "${NS_DATABASE}"
 ${NS_DOMAIN}. IN SOA ns1.${NS_DOMAIN}. hostmaster.${NS_DOMAIN}. 1674985166 10800 3600 604800 1800
 ${NS_DOMAIN}. 1800 IN NS ns1.${NS_DOMAIN}.
 EOF
+chown "${BIND_USER}:${BIND_USER}" "${NS_DATABASE}"
 
 # Add NS record to NS_DOMAIN zone
-for((i=1;i<="${NS_ADDR_COUNT}";i++)); do
-    record="NS${i}_ADDR"
+for((i=1;i<="${NS_SERVER_COUNT}";i++)); do
+    record="NS_${i}_SERVER"
     record="${!record}"
     echo "ns${i}     1800    IN  A   ${record}" >> "${NS_DATABASE}"
 done
 
+if [[ "${NS_ROLE}" == "primary" ]]; then
 cat <<EOF >> "${NAMED_CONF_FILE}"
 
 // Default name server zone ${NS_DOMAIN}
 zone "${NS_DOMAIN}." {
   type ${NS_ROLE};
   file "${NS_DATABASE}";
-  notify explicit;
+  notify yes;
 };
 EOF
+elif [[ "${NS_ROLE}" == "secondary" ]]; then
+cat <<EOF >> "${NAMED_CONF_FILE}"
+
+// Default name server zone ${NS_DOMAIN}
+zone "${NS_DOMAIN}." {
+  type ${NS_ROLE};
+  file "${NS_DATABASE}";
+  notify yes;
+  primaries { ${NS_1_SERVER}; };
+};
+EOF
+fi
 
 # Debug print
 if [[ -z "${DEBUG}" ]]; then
@@ -80,11 +96,20 @@ AVAILABLE_ZONES="${AVAILABLE_ZONES}"
 IFS=', ' read -r -a _AVAILABLE_ZONES <<< "${AVAILABLE_ZONES}"
 
 cat <<EOF >> "${NAMED_CONF_FILE}"
+
 //
 // Add you zones here
 //
 
 EOF
+
+#
+SECONDARY_SERVER_IPS=""
+for((i=2;i<="${NS_SERVER_COUNT}";i++)); do
+    record="NS_${i}_SERVER"
+    record="${!record}"
+    SECONDARY_SERVER_IPS="${SECONDARY_SERVER_IPS}\n    ${record};"
+done
 
 # Loop over AVAILABLE_ZONES list
 # Generate stub zone file for each domain
@@ -107,9 +132,10 @@ ${zone}	IN SOA	ns1.${NS_DOMAIN}. hostmaster.${zone}. (
 				)
 EOF
 # cat <<EOF > "${ZONE_DATABASE}"
+chown "${BIND_USER}:${BIND_USER}" "${ZONE_DATABASE}"
 
     # Add NS record to zone
-    for((i=1;i<="${NS_ADDR_COUNT}";i++)); do
+    for((i=1;i<="${NS_SERVER_COUNT}";i++)); do
         echo "			NS ns${i}.${NS_DOMAIN}." >> "${ZONE_DATABASE}"
     done
 
@@ -129,33 +155,28 @@ cat <<EOF >> "${NAMED_CONF_FILE}"
 zone "${zone}." {
   type ${NS_ROLE};
   file "${ZONE_DATABASE}";
-  notify explicit;
-  // IP addresses of secondary servers allowed to
-  // transfer the zone
+  notify yes;
   allow-transfer {
-    key "${OCTODNS_KEY_NAME}";
-EOF
-for((i=2;i<="${NS_ADDR_COUNT}";i++)); do
-    record="NS${i}_ADDR"
-    record="${!record}"
-    echo "    ${record};" >> "${NAMED_CONF_FILE}"
-done
-cat <<EOF >> "${NAMED_CONF_FILE}"
+    key "${OCTODNS_KEY_NAME}"; # AXFR
+    ${SECONDARY_SERVER_IPS}
+  };
+  allow-update {
+    key "${OCTODNS_KEY_NAME}"; # RFC 2136
+    ${SECONDARY_SERVER_IPS}
   };
 };
 
 EOF
 # Add zone to NAMED_CONF_FILE as secondary
-# Setup masters to NS1_ADDR
+# Setup masters to NS_1_SERVER
 elif [[ "${NS_ROLE}" == "secondary" ]]; then
 cat <<EOF >> "${NAMED_CONF_FILE}"
 // ${zone}
 zone "${zone}." {
   type ${NS_ROLE};
   file "${ZONE_DATABASE}";
-  notify explicit;
-  // IP address of eng.example.com primary server
-  masters { ${NS1_ADDR}; };
+  notify yes;
+  primaries { ${NS_1_SERVER}; };
 };
 
 EOF
