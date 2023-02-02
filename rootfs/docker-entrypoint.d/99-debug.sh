3b06f7e961e6a5a7b491b02accabbdbba682f796
Socheat Sok <socheatsok78@gmail.com>
2023-02-02 16:08:58 +0700

    Check if DEBUG ENV are set before print debug log

--- rootfs/docker-entrypoint.d/99-debug.sh
+++ rootfs/docker-entrypoint.d/99-debug.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
-set -eu
+set -e
 
-echo "=================================================="
-echo "Showing ${OCTODNS_KEY_FILE}:"
-echo "=================================================="
-cat "${OCTODNS_KEY_FILE}"
-echo ""
+if [[ -n "${DEBUG}" ]]; then
+    echo "=================================================="
+    echo "Showing ${OCTODNS_KEY_FILE}:"
+    echo "=================================================="
+    cat "${OCTODNS_KEY_FILE}"
+    echo ""
 
-echo "=================================================="
-echo "Showing ${NAMED_OPTIONS_FILE}:"
-echo "=================================================="
-cat "${NAMED_OPTIONS_FILE}"
-echo ""
+    echo "=================================================="
+    echo "Showing ${NAMED_OPTIONS_FILE}:"
+    echo "=================================================="
+    cat "${NAMED_OPTIONS_FILE}"
+    echo ""
 
-echo "=================================================="
-echo "Showing ${NAMED_CONF_FILE}:"
-echo "=================================================="
-cat "${NAMED_CONF_FILE}"
+    echo "=================================================="
+    echo "Showing ${NAMED_CONF_FILE}:"
+    echo "=================================================="
+    cat "${NAMED_CONF_FILE}"
+fi
