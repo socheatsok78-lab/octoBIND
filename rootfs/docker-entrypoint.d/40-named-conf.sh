a4d8bcc8d4042822c7c59679d636a9c2bbae0887
Socheat Sok <socheatsok78@gmail.com>
2023-02-04 19:33:06 +0700

    Refactor entrypoint scripts

--- rootfs/docker-entrypoint.d/40-named-conf.sh
+++ rootfs/docker-entrypoint.d/40-named-conf.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash
 set -e
 
+NAMED_CONF_FILE="${NAMED_CONF_FILE:-/etc/bind/named.conf.local}"
+NAMED_CONF_BACKUP_FILE="${NAMED_CONF_FILE}.origin"
+
+# Create backup for NAMED_CONF_FILE
+if [ ! -f "${NAMED_CONF_BACKUP_FILE}" ]; then
+	cp "${NAMED_CONF_FILE}" "${NAMED_CONF_BACKUP_FILE}"
+fi
+
+# Create a new NAMED_CONF_FILE from NAMED_CONF_BACKUP_FILE everytime the system boot
+rm "${NAMED_CONF_FILE}"
+cp "${NAMED_CONF_BACKUP_FILE}" "${NAMED_CONF_FILE}"
+
+# Nameserver
 NS_SERVER_DOMAIN="${NS_SERVER_DOMAIN:-nameserver.corpnet}"
 NS_SERVER_ROLE=${NS_SERVER_ROLE:-primary} # role: primary or secondary
 
