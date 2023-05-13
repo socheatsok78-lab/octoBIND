19c602f1cb23d7f9a50ab6d03e7d188133388de7
Socheat Sok <socheatsok78@gmail.com>
2023-05-13 15:11:35 +0700

    Fix debug mode cause error

--- rootfs/docker-entrypoint.d/99-debug.sh
+++ rootfs/docker-entrypoint.d/99-debug.sh
@@ -16,14 +16,19 @@ print_file_debug "/etc/bind/named.conf.options"
 print_file_debug "/etc/bind/named.conf.local"
 
 if [[ "${NS_SERVER_ROLE}" == "primary" ]]; then
-for col in /var/lib/bind/db.*; do
-    if [[ "${col}" != *".jnl" ]]; then
-        print_file_debug "$col"
+    if [[ -n "${ZONES_AVAILABLE}" ]]; then
+        for col in /var/lib/bind/db.*; do
+            if [[ "${col}" != *".jnl" ]]; then
+                print_file_debug "$col"
+            fi
+        done
     fi
-done
-for col in /var/lib/bind/*.rev; do
-    if [[ "${col}" != *".jnl" ]]; then
-        print_file_debug "$col"
+
+    if [[ -n "${ARPA_AVAILABLE}" ]]; then
+        for col in /var/lib/bind/*.rev; do
+            if [[ "${col}" != *".jnl" ]]; then
+                print_file_debug "$col"
+            fi
+        done
     fi
-done
 fi
